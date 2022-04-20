b0d9200528151c606a364927547ffddf9d427d70
dmagdaleno <diegommagdaleno@gmail.com>
2022-04-19 21:55:52 -0300

    adiciona status a exceção

--- src/main/java/br/com/fiap/abctechservice/handler/exception/CustomException.java
+++ src/main/java/br/com/fiap/abctechservice/handler/exception/CustomException.java
@@ -3,11 +3,13 @@ package br.com.fiap.abctechservice.handler.exception;
 import lombok.Getter;
 
 @Getter
-public class CustomException extends  RuntimeException{
-    private String description;
+public class CustomException extends RuntimeException{
+    private final int status;
+    private final String description;
 
-    public CustomException(String message, String description) {
+    public CustomException(int status, String message, String description) {
         super(message);
+        this.status = status;
         this.description=description;
     }
 }
